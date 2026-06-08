5f6be8ce565d812e5f9b09c3087a8e4fb262918f
heidexdev <heidexdev@gmail.com>
2026-06-08 17:47:47 +0330

    refactor playRound function to improve logging and game flow
    add playGame function , add a condition to determate the winner on pointes if nobody gets 3 score after 5 rounds . logging result and scores at the end

--- script.js
+++ script.js
@@ -18,21 +18,41 @@ let getHumanChoise = () => {
 let humanScore = 0;
 let computerScore = 0;
 
-let playRound = (humanChoise, computerChoise) => {
-  if (humanChoise == computerChoise) {
-    return "It's a tie ! no points to anybody.";
-  } else if (humanChoise == "rock" && computerChoise == "scissors") {
-    humanScore++;
-    return "Human Won ! | rock beats scissors ";
-  } else if (humanChoise == "paper" && computerChoise == "rock") {
-    humanScore++;
-    return "Human Won ! | paper beats rock ";
-  } else if (humanChoise == "scissors" && computerChoise == "paper") {
-    humanScore++;
-    return "Human Won ! | scissors beats paper ";
+let playGame = () => {
+  let playRound = (humanChoise, computerChoise) => {
+    if (humanChoise == computerChoise) {
+      return console.log("It's a tie ! no points to anybody.");
+    } else if (humanChoise == "rock" && computerChoise == "scissors") {
+      humanScore++;
+      return console.log("Human Won !  rock beats scissors ");
+    } else if (humanChoise == "paper" && computerChoise == "rock") {
+      humanScore++;
+      return console.log("Human Won !  paper beats rock ");
+    } else if (humanChoise == "scissors" && computerChoise == "paper") {
+      humanScore++;
+      return console.log("Human Won !  scissors beats paper ");
+    } else {
+      computerScore++;
+      return console.log("computer won this round !");
+    }
+  };
+
+  for (let i = 0; i < 5; i++) {
+    playRound(getHumanChoise(), getComputerChoise());
+    if (humanScore == 3) {
+      return console.log("Congrats ! Human won the Game ");
+    } else if (computerScore == 3) {
+      return console.log("You lost to the computer !");
+    }
+  }
+  if (humanScore > computerScore) {
+    return console.log("Game Over: Human wins on points!");
+  } else if (computerScore > humanScore) {
+    return console.log("Game Over: Computer wins on points!");
   } else {
-    computerScore++;
-    return "computer won this round !";
+    return console.log("Game Over: It's an overall tie!");
   }
 };
-
+let gameResult = playGame();
+console.log(gameResult);
+console.log("humanScore: " + humanScore + ", computerScore: " + computerScore);
